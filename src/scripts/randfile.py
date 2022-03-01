d2e294ef65180a5ced1fb32bf2eb2295c3feb234
Tucker Brouillard <tuckerbrouillard@outlook.com>
2022-03-01 12:02:35 -0500

    Append and overwrite both whole and part added

--- src/scripts/randfile.py
+++ src/scripts/randfile.py
@@ -1,11 +1,14 @@
 import argparse
+from logging import exception
+from tkinter import EXCEPTION
 import numpy
 import os.path
+from os import listdir
 from PIL import Image
 
 # Sample run command: python3 .\randfile.py -p .\temp\
-# TODO ADD FIND EXISTING FILES FUNCTION
-# TODO ADD CHOICE OF GS VS RGB IMG
+# TODO COPY, DELETE, SHRINK
+# TODO CARRY EXCEPTIONS UP FUNCTION DOCUMENTS? Single point of truth annoyance. Could say "exceptions of this sub function"
 
 # DATA GENERATION
 ###################################################################################################
@@ -37,7 +40,23 @@ def rand_pixels(cols, rows=1, color=True):
 
 # FILE ACTIONS
 ###################################################################################################
-def get_unused_file_names(path, extension, number=1, name_num_min=10000, name_num_max=99999):
+def get_used_file_names(path, extension, number=1):
+    """
+    TODO
+    """
+    # Get all files in path
+    files = [file for file in listdir(path) if os.path.isfile(path + file)]
+    # Filter by extension
+    files_of_type = []
+    for file in files:
+        if file[len(file) - len(extension):] == extension:
+            files_of_type.append(path + file)
+    if len(files_of_type) < number:
+        raise ValueError("Files of the given type in the given path are less than desired number")
+    return files_of_type
+
+# TODO COPY NAMES IMPLEMENTATION
+def get_unused_file_names(path, extension, number=1, name_num_min=10000, name_num_max=99999, copy=False):
     """
     Generate a list of numeric file names not currently in use at a given path with a given extension.
     Input:
@@ -46,6 +65,7 @@ def get_unused_file_names(path, extension, number=1, name_num_min=10000, name_nu
         number - The number of file names to get. Defaults to 1.
         name_num_min - The minimum number the name will be. Defaults to 10000.
         name_num_max - The maximum number the name will be. Defaults to 99999.
+        copy - Whether or not the new names are for copied files. Defaults to False.
     Output:
         The list of full file paths. Any path includes the given directory path, the generated file name, and the file extension.
     Raise:
@@ -67,35 +87,113 @@ def get_unused_file_names(path, extension, number=1, name_num_min=10000, name_nu
         name_num = name_num + 1
     return names
 
-def create_files(args):
+def _write_files(args, names, write_open_method = "w"):
     """
-    Creates files using the specified arguments.
+    Write files with given names using a given method and specified arguments.
     Input:
         args - The specified arguments.
+        names - The file names to write to.
+        write_open_method - The writing method specified for opening. Expects a, w, or x.
     Ouput:
-        Creates files.
+        Writes files.
         Returns nothing.
     Raise:
         Exception - If the extension requested is not implemented.
+        Exception - If the write_open_method is not supported.
     """
-    # Get file names
-    names = get_unused_file_names(args["path"], args["extension"], number=args["number"])
     # Generate data and write files depending on extension
     for name in names:
         if args["extension"] == "txt":
+            # Generate data
             data = rand_chars(args["size"])
+            # Only overwrite first characters
+            if write_open_method == "w":
+                f = open(name, "r")
+                old_data = list(f.read())
+                # If old data is larger than new data overwrite, otherwise just continue with new data
+                if len(old_data) > args["size"]:
+                    for i in range(args["size"]):
+                        old_data[i] = data[i]
+                    data = old_data
             # Write new file
-            f = open(name, "x")
+            f = open(name, write_open_method)
             for character in data:
                 f.write(character)
+            f.close()
         elif args["extension"] == "jpg" or args["extension"] == "png":
-            data = rand_pixels(rows=args["size"], cols=args["size"], color=True)
+            if write_open_method == "a":
+                # Open existing image
+                image = Image.open(name)
+                # Convert image to array
+                old_data = numpy.array(image)
+                # Generate data to expand old image by size in each dimension
+                ## Get pixels for expanded backwards L shape
+                ## m by m old extended by n in both dimensions
+                ## adds m by n rectangle right and an n by m + n rectangle bottom
+                rect_right = rand_pixels(rows=len(old_data), cols=args["size"], color=True)
+                rect_bottom = rand_pixels(rows=args["size"], cols=len(old_data) + args["size"], color=True)
+                ## Append right rectangle to the ends of rows
+                data = []
+                for i in range(len(rect_right)):
+                    joined_row = numpy.concatenate((old_data[i], rect_right[i]))
+                    data.append(joined_row)
+                ## Append bottom rectangle unerneath rows
+                data.extend(rect_bottom)
+                data = numpy.asarray(data)
+            elif write_open_method == "w":
+                # Open existing image
+                image = Image.open(name)
+                # Convert image to array
+                old_data = numpy.array(image)
+                # Generate data to overwrite
+                data = rand_pixels(rows=args["size"], cols=args["size"], color=True)
+                # If old data is larger than new data overwrite, otherwise just continue with new data
+                if len(old_data) > args["size"]:
+                    for i in range(args["size"]):
+                        for j in range(args["size"]):
+                            old_data[i][j] = data[i][j]
+                    data = old_data
+            elif write_open_method == "x":
+                # Generate data
+                data = rand_pixels(rows=args["size"], cols=args["size"], color=True)
+            else:
+                raise Exception("Unexpected write method used")
             # Convert Pixels to image
             image = Image.fromarray(data, mode="RGB")
             # Write new file
             image.save(name)
         else:
             raise Exception("Unimplemented extension specified")
+    return
+
+def create_files(args):
+    """
+    TODO
+    """
+    # Get new file names
+    names = get_unused_file_names(args["path"], args["extension"], number=args["number"])
+    # Write with x to ensure new file
+    _write_files(args, names, write_open_method="x")
+    return
+
+def overwrite_files(args):
+    """
+    TODO
+    """
+    # Get existing file names
+    names = get_used_file_names(args["path"], args["extension"], number=args["number"])
+    # Write with w to overwrite
+    _write_files(args, names, write_open_method="w")
+    return
+
+def append_files(args):
+    """
+    TODO
+    """
+    # Get existing file names
+    names = get_used_file_names(args["path"], args["extension"], number=args["number"])
+    # Write with a to append
+    _write_files(args, names, write_open_method="a")
     return
 
 # MAIN
@@ -164,6 +262,10 @@ def main():
     # Handle file action
     if args["mode"] == "create":
         create_files(args)
+    elif args["mode"] == "overwrite":
+        overwrite_files(args)
+    elif args["mode"] == "append":
+        append_files(args)
     else:
         raise Exception("Unimplemented mode specified")
     return
