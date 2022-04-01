7f0ad15d051e63ffa25efd7f7e288d1438a72663
walambrecht <lambrecht.w@husky.neu.edu>
2022-04-01 16:30:29 -0400

    Updated Docker-Compose for ELK stack to not have a Read Only ElasticSearch
    filebeat now functioning using bash script to send OSQuery logs to logstash.
    
    Updated osquery.conf to utilize File integrity Management

--- src/scripts/logging/log_setup.sh
+++ src/scripts/logging/log_setup.sh
@@ -4,6 +4,7 @@
 apt-get install gnupg
 apt-get install software-properties-common
 apt install linux-libc-dev
+apt-get update; apt-get install curl
 apt-get update
 
 # Install OSQuery
@@ -21,10 +22,22 @@ cp osquery.conf /etc/osquery/
 cp osquery.flags /etc/osquery/
 cp -R packs /etc/osquery/
 
-# Check configuration
+# Check configurations
 osqueryctl config-check
 
-# Run the OSQuery Daemon on scheduled intervals
-osqueryd "--verbose"
+# Install Filebeat
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-oss-7.15.1-amd64.deb
+dpkg -i filebeat-oss-7.15.1-amd64.deb
 
-# Logs are stored here: /var/log/osquery/osqueryd.results.log
+# Setup Filebeat
+chown root:root /etc/filebeat/filebeat.yml
+cp filebeat.yml /etc/filebeat/filebeat.yml
+filebeat -e -c /etc/filebeat/filebeat.yml
+
+# Run the OSQuery Daemon on scheduled intervals
+osqueryd
+
+# Run Filebeat
+./bin/filebeat -e
+
+# Logs are stored on each machine locally: /var/log/osquery/osqueryd.results.log
