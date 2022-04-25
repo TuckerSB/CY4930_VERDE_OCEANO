6b9c4e15a9c81fbdb3846416220936e73ce53b44
Tucker Brouillard <tuckerbrouillard@outlook.com>
2022-04-25 19:23:37 -0400

    Fixed 0 SYN_RECV bug

--- src/scripts/logging/tcpdump/detect_synflood.sh
+++ src/scripts/logging/tcpdump/detect_synflood.sh
@@ -3,6 +3,9 @@ apt install net-tools
 while true
 do
     SYN_RECV=$(netstat -npt | awk '{print $6}' | sort | uniq -c | sort -nr | head | grep "SYN_RECV" | grep -o -E "[0-9]+")
+    if [[ "$SYN_RECV" == "" ]]; then
+        SYN_RECV=0
+    fi
     echo "{\"name\":\"network_connections\",\"SYN_RECV\":$SYN_RECV}" >> /var/log/network_connections.json
     sleep 60
 done
