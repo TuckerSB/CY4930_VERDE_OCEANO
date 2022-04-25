a06d979fcf4ac0faecb41c982d9138ffb12c9f34
Tucker Brouillard <tuckerbrouillard@outlook.com>
2022-04-25 18:55:13 -0400

    syn flood detect appends

--- src/scripts/logging/tcpdump/detect_synflood.sh
+++ src/scripts/logging/tcpdump/detect_synflood.sh
@@ -3,6 +3,6 @@ apt install net-tools
 while true
 do
     SYN_RECV=$(netstat -npt | awk '{print $6}' | sort | uniq -c | sort -nr | head | grep "SYN_RECV" | grep -o -E "[0-9]+")
-    echo "{\"name\":\"network_connections\",\"SYN_RECV\":$SYN_RECV}" > /var/log/network_connections.json
+    echo "{\"name\":\"network_connections\",\"SYN_RECV\":$SYN_RECV}" >> /var/log/network_connections.json
     sleep 60
 done
