90baed5d9b7bb179c0b0ac063191e9bbf3e62ed8
Tucker Brouillard <tuckerbrouillard@outlook.com>
2022-04-25 18:51:12 -0400

    Added detect syn flood script

---
+++ src/scripts/logging/tcpdump/detect_synflood.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt install net-tools
+while true
+do
+    SYN_RECV=$(netstat -npt | awk '{print $6}' | sort | uniq -c | sort -nr | head | grep "SYN_RECV" | grep -o -E "[0-9]+")
+    echo "{\"name\":\"network_connections\",\"SYN_RECV\":$SYN_RECV}" > /var/log/network_connections.json
+    sleep 60
+done
